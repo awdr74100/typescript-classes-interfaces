305ad48b13a33af632ae8f45c88afec0f509512b
Yi-Ru Lan <a78945612385238@gmail.com>
2022-07-07 03:37:01 +0800

    Update progress

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {}
 
@@ -33,6 +33,11 @@ class AccountingDepartment extends Department {
     super(id, 'Accounting');
   }
 
+  addEmployee(name: string) {
+    if (name === 'Max') return;
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -59,7 +64,11 @@ const accounting = new AccountingDepartment('d2', []);
 
 accounting.addReport('Something went wrong...');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Sharon');
+
 accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: 'Working', describe: accounting.describe };
 
