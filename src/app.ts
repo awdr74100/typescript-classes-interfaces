5b3fe6d379493833e94d107c8addada15a1c1036
Yi-Ru Lan <a78945612385238@gmail.com>
2022-07-07 19:01:20 +0800

    Update progress

--- src/app.ts
+++ src/app.ts
@@ -29,8 +29,21 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private latestReport: string;
+
+  get mostRecentReport() {
+    if (this.latestReport) return this.latestReport;
+    else throw new Error('No report found.');
+  }
+
+  set mostRecentReport(value: string) {
+    if (value) this.addReport(value);
+    else throw new Error('Please pass in a valid value.');
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.latestReport = reports[reports.length - 1] || '';
   }
 
   addEmployee(name: string) {
@@ -40,6 +53,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.latestReport = text;
   }
 
   printReports() {
@@ -62,7 +76,10 @@ console.log(it);
 
 const accounting = new AccountingDepartment('d2', []);
 
+accounting.mostRecentReport = 'Year End Report';
 accounting.addReport('Something went wrong...');
+
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Sharon');
