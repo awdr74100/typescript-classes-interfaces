dd8edc8cc8f194e2cb73990334dc07f28cd09d22
Yi-Ru Lan <a78945612385238@gmail.com>
2022-07-13 04:42:31 +0800

    Update progress

--- src/app.ts
+++ src/app.ts
@@ -8,25 +8,28 @@ let add: AddFn;
 add = (a, b) => a + b;
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
-  greet(phrase: string): void;
+  greet?(phrase: string): void;
 }
 
 class Person implements Greetable {
-  constructor(public name: string, public age = 30) {}
+  constructor(public name?: string, public age = 30) {
+    if (name) this.name = name;
+  }
 
   greet(phrase: string) {
-    console.log(`${phrase} ${this.name}`);
+    this.name ? console.log(`${phrase} ${this.name}`) : console.log('Hi!');
   }
 }
 
 let user: Greetable;
 
-user = new Person('Max');
+user = new Person();
 // user.name = 'Leo';
 
-user.greet('Hi there - I am');
+user.greet?.('Hi there - I am');
 console.log(user);
